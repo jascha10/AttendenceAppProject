646601b2ec19cab5d748bce7038c7a8b222f0bff
jascha10 <jascha10@wsc.edu>
2025-03-03 09:44:12 -0600

    Added displayappheadding method

--- src/AttendanceApp.java
+++ src/AttendanceApp.java
@@ -20,8 +20,26 @@ public class AttendanceApp {
      */
     private final static String SINGLE_LINE = "-----------------------------------------";
 
+    /**
+     * Default Constructor that instantiate 2 courses.
+     */
     public AttendanceApp() {
         section1 = new Course();
         section2 = new Course();
+    }
+
+    /**
+     *  Displays the AttendanceApp header. Example:<br>
+     *  <pre>
+     *  =========================================
+     *  Welcome to the Attendance App
+     *  =========================================
+     *  </pre>
+     */
+    private void displayAppHeading() {
+        System.out.println(DOUBLE_LINE);
+        System.out.println("Welcome to the Attendance App");
+        System.out.println(DOUBLE_LINE);
+        System.out.println();
     }
 }
