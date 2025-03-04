ceaff91a2ff470e493f867ca423cb713edcc3202
jacob <jascha10@wsc.edu>
2025-03-03 18:45:43 -0600

    Have all the proper code just need to get it all to work.

--- src/AttendanceApp.java
+++ src/AttendanceApp.java
@@ -59,22 +59,115 @@ public class AttendanceApp {
         setupCourses();
     }
 
-    private void setupSections() {
+    private void setupStudents(Course course) {
+        String courseName = course.getName();
+        String name;
+        int seat;
 
-    }
-    private void mainMenu() {
+        while (true) {
+            System.out.println();
+            name = Input.getLine("Enter " + courseName + " student's name or 'q' to quit: ");
 
-    }
+            if (name.equals("q"))
+                return;
+
+            try {
+                seat = Input.getIntRange("Enter " + name + "'s seat number: ", 0, 30); .
+                addStudent(name, seat);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                System.out.println("Unable to add student!");
+            }
+
+        }
+
+    } // end of setupPlayers
+    private void mainMenu() throws Exception {
+
+        boolean playGame = true;
+        int userInput;
+
+        System.out.println();
+        System.out.println(DOUBLE_LINE);
+        System.out.println("Game Started!");
+        System.out.println(DOUBLE_LINE);
+        System.out.println();
+
+
+        while (playGame) {
+
+            System.out.println(SINGLE_LINE);
+            System.out.println("Main Menu");
+            System.out.println(SINGLE_LINE);
+
+            System.out.println("0 = End Game");
+            System.out.println("1 = Enter " + section1.getName() + " Team's Stats");
+            System.out.println("2 = Enter " + section2.getName() + " Team's Stats");
+            System.out.println("3 = Display Game Stats");
+
+            System.out.println(SINGLE_LINE);
+            userInput = Input.getIntRange("Menu Choice: ", 0, 3);
+            System.out.println(SINGLE_LINE);
+
+            System.out.println();
+
+            switch (userInput) {
+                case 0:
+                    playGame = false;
+                    updatedAttendence();
+                    System.out.println();
+                    break;
+
+                case 1:
+                case 2:
+
+                    if (userInput == 1)
+                        updateTeamStats(section1);
+                    else
+                        updateTeamStats(section2);
+
+                    System.out.println();
+                    updatedAttendence();
+                    System.out.println();
+                    break;
+
+                case 3:
+                    displayGameStatus();
+                    break;
+
+                default:
+                    System.out.println("Invalid menu choice = " + userInput);
+
+            } // end of switch
+        }
+
+    } // end of playGame
+
     private void courseAttendence() {
-
+        section1.displaySummaryReport();
+        section2.displaySummaryReport();
     }
     private void studentAttendence() {
-
+        section1.displayDetailReport();
+        section2.displayDetailReport();
     }
     private void displayDetailReports() {
 
     }
     public static void main(String[] args) {
 
+        AttendanceApp attend = new AttendanceApp();
+
+        attend.displayDetailReports();
+
+        try {
+            attend.setupCourses();
+            attend.setupStudents();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Sorry but this program ended with an error. Please contact Jacob!");
+        }
+
+        Input.sc.close();
     }
 }
